dcafd70d517facfc3481848af9f9eab77aa0f578
Jasoncarterjoseph <46191199+Jasoncarterjoseph@users.noreply.github.com>
2021-07-16 21:35:50 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect,useState } from "react";
+import Editor from "./components/Editor";
+import TopNavigation from './TopNavigation'
 
-function App() {
+const App = () => {
+  const [html, setHtml] = useState("");
+  const [css, setCss] = useState("");
+  const [js, setJs] = useState("");
+  const [srcDoc, setSrcDoc] = useState("");
+  
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(
+       `<html>
+        <body>${html}</body>
+        <style>${css}</style>
+        <script>${js}</script>
+        </html>`);
+    }, 250);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [html, css, js]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+       <TopNavigation />
+      </div>
+      <div className="pane top-pane">
+       <Editor FontAwesomeIcon title="HTML" language="xml" value={html} onChange={setHtml}/>
+       <Editor title="CSS" language="css" value={css} onChange={setCss}/>
+       <Editor title="JS" language="javascript" value={js} onChange={setJs}/>
+      </div>
+      <div className="pane">
+      <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameborder="0"
+          width="100%"
+          height="100%"
+        />      
+      </div>
+    </>
   );
-}
+};
 
 export default App;
